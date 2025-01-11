96aaafabb1bb314c489b6ba699e459b8fe9e8b07
yashp386 <yashp30386@gmail.com>
2025-01-11 23:43:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', function(req, res) {
-  res.send('Hello World!')
+app.get('/', function(req, res) {   // creats a server to port any js file to the browser
+  res.send('Hello World!')    
 })
 
 app.listen(port)
